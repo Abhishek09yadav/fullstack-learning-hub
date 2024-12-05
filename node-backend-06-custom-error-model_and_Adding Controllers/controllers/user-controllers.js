bda55f4c03b241f51583a5cf44f4e39ff9369d56
Abhishek09yadav <monuy8830@gmail.com>
2024-12-05 23:00:45 +0530

    Implements user signup routes.

--- node-backend-06-custom-error-model_and_Adding Controllers/controllers/user-controllers.js
+++ node-backend-06-custom-error-model_and_Adding Controllers/controllers/user-controllers.js
@@ -16,12 +16,16 @@ const signup = (req,res,next)=>{
     const createUser  = {
         id:uuid4(),userName, password, email
     }
+    const UserExist = DUMMY_USERS.find(u => u.email === email);
+    if(UserExist){
+        return res.status(401).send({message: "user Exists please login"});
+    }
     DUMMY_USERS.push(createUser);
     res.status(201).json({DUMMY_USERS})
 }
 const login = (req,res,next) =>{
     const {email,password} = req.body;
-const identifiedUser =
+    const identifiedUser =
     DUMMY_USERS.find(p =>{
        return p.email === email && p.password === password
     })
