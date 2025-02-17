7bd8800e4a11751ae8bd9825356c78f3ff5593c6
Abhishek09yadav <monuy8830@gmail.com>
2025-02-17 17:22:53 +0530

    Implement backend authentication routes and database connection

---
+++ RealTimeChatApplication/backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+const PORT = process.env.PORT || 5999;
+const app = express();
+dotenv.config();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+  connectDB();
+});
+app.get("/", (req, res) => res.send(`server is running on port ${PORT}`));
+app.use("/api/auth", authRoutes);
