6e4654cbcf612ec4ca0471d52044d77352a5b61c
Abhishek09yadav <monuy8830@gmail.com>
2025-02-19 10:45:21 +0530

    Refactor signup function to improve password handling and formatting

--- RealTimeChatApplication/backend/src/controllers/auth.controller.js
+++ RealTimeChatApplication/backend/src/controllers/auth.controller.js
@@ -1,19 +1,23 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const signup = async (req,res) =>{ 
-  const {email,fullname,password} =req.body;
+export const signup = async (req, res) => {
+  const { email, fullname, password } = req.body;
   try {
-    if(password.length < 6  ) res.status(400).json({message:"Password must be at least 6 characters long"});
-    const user = await User.findOne({email});
-    if(user) return res.status(400).json({message:"User already exists"});
-
-    const newUser = new User({email,fullname,password});
-
-    res.status(201).json({message:"User created successfully",user});
+    if (password.length < 6)
+      res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword  = bcrypt.hash(password,salt)
   } catch (error) {
     console.log(error);
   }
-
-}
-export const login = (req,res) =>{  res.send("login");}
-export const logout = (req,res) =>{  res.send("logout");}
+};
+export const login = (req, res) => {
+  res.send("login");
+};
+export const logout = (req, res) => {
+  res.send("logout");
+};
