546f1f09c9e2a1fec943d576dbf285f5e700bbb8
Abhishek Yadav <monuy8830@gmail.com>
2025-05-22 17:57:31 +0530

    feat: add ChildComponent and integrate it into Home component with todo functionality

--- frontend/rerendering/src/app/page.js
+++ frontend/rerendering/src/app/page.js
@@ -1,17 +1,47 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import ChildComponent from "./ChildComponent";
 
 export default function Home() {
-  const HandleAdd =() =>{}
-  const Toggle =(id) =>{
+  const [todos, setTodos] = useState([]);
+  const [name, setName] = useState("");
+  const HandleAdd = (e) => {
+    const newTodo = {
+      id: Date.now(),
+      name: name,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setName("");
+  };
+  const Toggle = (id) => {
+    const newTodos = todos.map((todo) => {
+      todo.id === id ? todo.completed = !todo.completed : todo.completed;
+      return todo;
+    })
+    setTodos(newTodos) 
+  };
+  const Delete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-  }
-  const Delete =(id) =>{
-
-  }
-  
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <ChildComponent />
+      <div className="container my-auto mx-auto mt-3  ">
+        <div className="text-4xl text-center font-medium underline">sbd</div>
+        <div className="justify-center items-center gap-2 flex flex-row no-wrap">
+          <input type="text" className="p-3 mt-3  border-3 border-gray-400 rounded-full" value={name} onChange={(e) =>setName(e.target.value)}   />
+          <button className="p-3 rounded-full bg-blue-500 hover:bg-blue-600 " onClick={() => HandleAdd()} >Add</button>
+        </div>
+        <ul>
+          {
+            todos.map((todo) =>{
+              <li key={todo.id} > {todo.name} </li>
+            })
+          }
+        </ul>
+      </div>
+    </>
   );
 }
