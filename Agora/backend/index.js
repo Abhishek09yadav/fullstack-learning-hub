2e17f4665815855592a5670d4e9093af532ef364
Abhishek09yadav <monuy8830@gmail.com>
2025-05-29 01:32:46 +0530

    feat: add votes and words arrays; implement counting and joining logic

--- Agora/backend/index.js
+++ Agora/backend/index.js
@@ -1,7 +1,9 @@
 const dishes = ["plate", "bowl", "cup", "fork", "knife"];
+const votes = ["pizza", "burger", "pizza", "salad", "burger", "pizza"];
 const oddEven = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
+const words = ["JavaScript", "is", "fun"];
 const mapres = dishes.map((item, index) => {
   return item + " " + index;
 });
@@ -13,9 +15,23 @@ const forEachRes = dishes.forEach((dish) => {
   console.log(dish);
 });
 const initialValue = 0;
-const reduceRes = oddEven.reduce((acc, currValue) => {
-  return acc + currValue
-}, initialValue);
+const reduceRes = oddEven.reduce((acc,curr)=>{
+  return acc+curr;
+},initialValue)
+
+const countArr = votes.reduce((acc,curr)=>{
+  acc[curr] = (acc[curr] || 0 ) + 1
+  return acc 
+},{})
+// const wordRes = words.join(" ")
+const wordsRes = words.reduce((acc,curr)=>{
+  return acc +" "+ curr
+})
+
+
+
 console.log("map res ", mapres);
 console.log("filter res ", filterres);
 console.log("reduce res ", reduceRes);
+console.log("count res ", countArr);
+console.log("word res ", wordsRes);
