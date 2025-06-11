9c024897d17f3b7170121e2d36969726f1c5ecc8
Abhishek Yadav <monuy8830@gmail.com>
2025-06-11 14:14:06 +0530

    123

--- backend/routeParams/index.js
+++ backend/routeParams/index.js
@@ -3,7 +3,9 @@ import userRoutes from './routes/userRoutes.js'
 const app = express();
 const port = 4000
 app.use('/api',userRoutes)
-
+app.get('/', (req,res)=>{
+  res.status(200).send({messge:"hello"})
+})
 
 
 app.listen(port, () => {console.log(`Server is running on http://localhost:${port}`)})
