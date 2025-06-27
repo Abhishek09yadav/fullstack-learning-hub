0449bb9dfb818f926ba2217e237e4aec366a49ea
Abhishek Yadav <monuy8830@gmail.com>
2025-06-27 17:31:05 +0530

    123

--- backend/ls/controllers/loginController.js
+++ backend/ls/controllers/loginController.js
@@ -2,15 +2,16 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken'
+import Users from '../models/Users.js'
 
 
-export const signup = (req, res) => {
+export const signup =async (req, res) => {
   const { fullName, email, password } = req.body;
 
   if (!fullName || !email || !password) {
     res.status(500).json({ message: "please fill all information" });
   }
-  const existingUser = Users.findOne({})
+  const existingUser = await Users.findOne({})
 
 
 };
--- backend/ls/index.js
+++ backend/ls/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from './routes/loginRoute.js'
 dotenv.config();
 const PORT = 4000;
 
@@ -14,6 +15,6 @@ mongoose
   .then(() => console.log("mongodb connected"))
   .catch((e) => console.log("error connecting to mongodb ", e));
 app.use("/", (req, res) => res.status(200).json({ message: "hello user " }));
-// app.use('/api/auth',authRoute)
+app.use('/api/auth',authRoute)
 
 app.listen(PORT, () => console.log("server is running on port", PORT));
