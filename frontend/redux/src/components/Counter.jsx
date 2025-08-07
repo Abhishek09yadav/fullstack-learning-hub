088a6e07e68f97614296e32fe9098deb14490fd4
Abhishek09yadav <monuy8830@gmail.com>
2025-08-07 23:10:30 +0530

    Comment out console.log and fix onClick handlers in Counter component

--- frontend/redux/src/components/Counter.jsx
+++ frontend/redux/src/components/Counter.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-  console.log("count", count);
+  // console.log("count", count);
   const handleIncrement = () => {
     dispatch(increment())
   };
@@ -15,11 +15,11 @@ const Counter = () => {
   };
   return (
     <div className="flex flex-row container  justify-center items-center gap-2 mt-3">
-      <button className="bg-blue-500 p-8 rounded-md" onClick={handleIncrement}>
+      <button className="bg-blue-500 p-8 rounded-md" onClick={() =>handleIncrement}>
         +
       </button>
         <div className="border bg-gray-200 rounded-md p-6 " >Count: {count}</div>
-      <button className="bg-blue-500 p-8 rounded-md" onClick={handleDecrement}>
+      <button className="bg-blue-500 p-8 rounded-md" onClick={() =>handleDecrement}>
         -
       </button>
     </div>
