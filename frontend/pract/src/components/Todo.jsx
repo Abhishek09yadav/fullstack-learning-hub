996e0bd4e2004546948fc94140bd5d153d228061
Abhishek09yadav <monuy8830@gmail.com>
2025-09-22 23:37:43 +0530

    Enhance Todo component with description field and improved data structure

--- frontend/pract/src/components/Todo.jsx
+++ frontend/pract/src/components/Todo.jsx
@@ -1,38 +1,67 @@
 "use client";
 import React, { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
 
 const Todo = () => {
-  const [data, setData] = useState(null);
+  const initial = {
+    title: "",
+    desc: "",
+    status: "pending",
+    Date: Date.now(),
+  };
+
+  const [data, setData] = useState(initial);
   const [todo, setTodo] = useState([]);
-  // const initial = [{
-  //   title:"",
-  // }]
+
   const handleAddTodo = () => {
-    setTodo(data);
-    setData(null);
+    if (!data.title.trim() || !data.desc.trim()) return;
+    setTodo((prev) => [...prev, data]);
+    setData(initial);
   };
   const deleteTodo = (index) => {};
   return (
     <div className="flex flex-col min-h-screen place-items-center bg-amber-50 ">
       <h1 className="font-bold text-3xl text-blue-300 my-4">Todo App...</h1>
-      <div className="flex gap-2 ">
-        {" "}
+      <div className="flex flex-col gap-2 ">
         <input
           className="w-full p-2 md:w-4xl border-2 border-blue-400 rounded-md "
           placeholder="add todo here"
-          onChange={(e) => setData(e.target.value)}
-          value={data}
+          onChange={(e) =>
+            setData((prev) => ({ ...prev, title: e.target.value }))
+          }
+          value={data.title}
         />
-        <button className="btn btn-primary flex place-items-center gap-2">
-          Add Todo <FaPlusCircle className="" onClick={handleAddTodo} />
+        {data.title && (
+          <textarea
+            className="w-full p-2 md:w-4xl border-2 border-blue-400 rounded-md "
+            placeholder="add todo desc here"
+            onChange={(e) =>
+              setData((prev) => ({ ...prev, desc: e.target.value }))
+            }
+            value={data.desc}
+          />
+        )}
+        <button
+          className=" btn btn-primary  mx-auto flex place-items-center gap-2 
+              disabled:cursor-not-allowed disabled:bg-gray-400"
+          disabled={!data.title || !data.desc}
+          onClick={handleAddTodo}
+        >
+          Add Todo <FaPlusCircle className="" />
         </button>
       </div>
       <div className="">
         {todo.length > 0 && (
-          <div className="">
+          <div className="flex flex-col max-w-lg">
             {todo.map((value, index) => (
-              <div key={index}>{value}</div>
+              <div className={"flex justify-between rounded-md "} key={index}>
+                <div className="text-xl font-bold ">{value.title}</div>
+                <div className="flex  ">
+                  <div className="text-blue-500"></div>
+                  <div className="text-red-400"></div>
+                </div>
+              </div>
             ))}
           </div>
         )}
