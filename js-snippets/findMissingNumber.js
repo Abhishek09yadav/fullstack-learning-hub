3f427cfd113cd37b8e4bb8614cc5b11128b62251
Abhishek09yadav <monuy8830@gmail.com>
2025-09-23 18:03:11 +0530

    Add frequency counter function and refactor findMissingNumber reduce callback

--- js-snippets/findMissingNumber.js
+++ js-snippets/findMissingNumber.js
@@ -1,8 +1,5 @@
 function findMissingNumber(arr, n) {
-  let total = arr.reduce((acc, initial) => {
-    acc = acc + initial;
-    return acc;
-  }, 0);
+  let total = arr.reduce((acc, initial) => (acc = acc + initial), 0);
   const sum = (n * (n + 1)) / 2;
   return sum - total;
 }
---
+++ js-snippets/frequencyOfElements.js
@@ -0,0 +1,14 @@
+function frequency(arr){
+  let obj = {}
+  for(let i = 0; i< arr.length; i++){
+    if(!obj[arr[i]]){
+      obj[arr[i]] = 1
+    }
+    else{
+      obj[arr[i]]++
+    }
+  }
+  return obj
+}
+
+console.log(frequency([1, 1, 2, 3, 3, 4]));
