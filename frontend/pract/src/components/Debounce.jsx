0a4b03378e43b7c6414d5f23feb32331b221632d
Abhishek09yadav <monuy8830@gmail.com>
2025-10-09 22:41:32 +0530

    Refactor debounce component: increase timeout to 3.5s and inline onChange handler

--- frontend/pract/src/components/Debounce.jsx
+++ frontend/pract/src/components/Debounce.jsx
@@ -3,16 +3,12 @@ import React, { useEffect, useState } from "react";
 
 const Debounce = () => {
   const [input, setInput] = useState(null);
-  useEffect(() => {
-    // console.log("useeffect triggered");
-    const time = setTimeout(() => {
-      console.log("api called: data: ",input);
-    }, [1000]);
-    return () => clearTimeout(time);
-  }, [input]);
-  const handleChange = (e) => {
-    setInput(e.target.value);
-  };
+  useEffect(()=>{
+    const timeoutId = setTimeout(() =>{
+      console.log("api called",input);
+    },[3500])
+    return () => clearTimeout(timeoutId)
+  },[input])
   return (
     <div className="min-h-screen flex flex-col bg-amber-50 justify-center items-center m-2">
       <h1 className="text-3xl font-extrabold text-blue-500">
@@ -20,7 +16,7 @@ const Debounce = () => {
       </h1>
       <input
         className="w-full md:w-1/4 cursor-pointer border rounded-md mt-4 boder-2 foucs:ring-2 p-2 "
-        onChange={handleChange}
+        onChange={(e) => setInput(e.target.value)}
         type="text"
         value={input}
       />
